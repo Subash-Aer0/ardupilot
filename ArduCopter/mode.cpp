6f0eac03e064172e381366a22873ec8e00b05849
Subash <ae23s004@smail.iitm.ac.in>
2024-09-22 13:06:59 +0530

    Mode Fig Inf climb

--- ArduCopter/mode.cpp
+++ ArduCopter/mode.cpp
@@ -158,6 +158,12 @@ Mode *Copter::mode_from_mode_num(const Mode::Number mode)
             break;
 #endif
 
+#if MODE_FIGINF_ENABLED
+        case Mode::Number::FIGINF:
+            ret = &mode_figinf;
+            break;
+#endif
+
         default:
             break;
     }
